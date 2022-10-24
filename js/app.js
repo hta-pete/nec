335999f580df9027ee60675c2753ce1a9d0184e5
hta-pete <pkatsaboulas@hightoweragency.com>
2022-10-24 08:29:18 -0500

    homepage

--- js/app.js
+++ js/app.js
@@ -15,8 +15,6 @@ $(function(){
   var lastScrollTop = $('main').offset().top;
 
   $window.on('load', function(){
-    checkScrollPos();
-    $('.hero').addClass('show');
     window.dispatchEvent(new Event('resize'));
   });
 
@@ -36,76 +34,40 @@ $(function(){
       lastScrollTop = scrollTop;
       raf(loop);
     }
-    if( scrollTop === 0 ) {
-      $('header').removeClass('has-shadow');
-    } else{
-      $('header').addClass('has-shadow');
-    }
 
-    if( inView( $('.what-we-do') ) ){
-      $carousel.flickity('pausePlayer');
-      $carousel2.flickity('pausePlayer');
-    } else{
-      $carousel.flickity('unpausePlayer');
-      $carousel2.flickity('unpausePlayer');
-    }
     lastScrollTop = scrollTop;
-    checkHiddenStuff();
+   
   }
 
-  function checkHiddenStuff(){
-    $('.scroll-in').each(function(){
-      if ( inView($(this).parent()) ) {
-        $(this).addClass('show');
-      } 
-    });
-    $('.has-images').each(function(){
-      if ( inView($(this)) ) {
-        $(this).addClass('reveal');
-      } 
-    });
-  }
-  
-  function checkScrollPos(){
-    setTimeout(function(){
-      checkHiddenStuff();
-    },500); 
-  };
-  checkScrollPos();
 
-  function inView(elem){
-    if (elem.length){
-      var centerY = Math.max(0,(($window.height() - elem.outerHeight()/2) ) + $window.scrollTop());
-      var elementTop = elem.offset().top;
-      return elementTop <= centerY;
+  // History State
+  /*
+  var $wrap = $("#wrap");
+
+  $wrap.on( "click", ".page-link", function(event) { 
+    event.preventDefault();
+    if (window.location === this.href) {
+      return;
     } 
-  }
-  // —————————
-  // Sliders
-  // —————————
-  var elem = document.querySelector('.hero-slider');
-  if(typeof(elem) != 'undefined' && elem != null){
-    var $carousel = $('.hero-slider').flickity({
-      draggable: false,
-      prevNextButtons: false,
-      pageDots: false,
-      wrapAround: true,
-      autoPlay: 5000,
-      pauseAutoPlayOnHover: false 
-    });
-  }
+    var pageTitle = (this.title) ? this.title : this.textContent;   
+    History.pushState( null, pageTitle, this.href );
+  });
 
-  var mask = document.querySelector('.mask');
-  if(typeof(mask) != 'undefined' && mask != null){
-    var $carousel2 = $('.mask').flickity({
-      draggable: false,
-      prevNextButtons: false,
-      pageDots: false,
-      wrapAround: true,
-      autoPlay: 5000,
-      pauseAutoPlayOnHover: false
+  History.Adapter.bind( window, "statechange", function() {  
+    var state = History.getState();  
+    $.get( state.url, function(res) {
+      $.each( $( res ), function(index, elem) {
+        if ( $wrap.selector !== "#" + elem.id ) {
+          return;
+        }
+        $wrap.html($( elem ).html());
+      });
     });
-  }
+  });
+  */
+
+ 
+
     
 });
 
